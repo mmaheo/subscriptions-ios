222024517112f90e5538edbabde32249bf559c44
Maxime Maheo <mmaheo@deezer.com>
2021-02-17 09:58:32 +0100

    Create project

---
+++ Shared/SubscriptionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionsApp.swift
+//  Shared
+//
+//  Created by Maxime Maheo on 17/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
